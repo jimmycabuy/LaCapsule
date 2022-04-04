aaa6a4f99e2e7dfc44a022b6e749f1fb48ddd562
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-04-04 15:50:10 +0200

    project done

--- projet/weatherapp/part1/routes/index.js
+++ projet/weatherapp/part1/routes/index.js
@@ -2,20 +2,20 @@ var express = require("express");
 var router = express.Router();
 
 var cityList = [
-  {
-    nom: "Paris",
-    image: "/images/picto-1.png",
-    descriptif: "Nuageux",
-    tempMin: 7,
-    tempMax: 12,
-  },
-  {
-    nom: "Lyon",
-    image: "/images/picto-1.png",
-    descriptif: "Nuageux",
-    tempMin: 4,
-    tempMax: 10,
-  }
+  // {
+  //   nom: "Paris",
+  //   image: "/images/picto-1.png",
+  //   descriptif: "Nuageux",
+  //   tempMin: 7,
+  //   tempMax: 12,
+  // },
+  // {
+  //   nom: "Lyon",
+  //   image: "/images/picto-1.png",
+  //   descriptif: "Nuageux",
+  //   tempMin: 4,
+  //   tempMax: 10,
+  // },
 ];
 
 router.get("/", function (req, res, next) {
@@ -23,6 +23,23 @@ router.get("/", function (req, res, next) {
 });
 
 router.get("/weather", function (req, res, next) {
+  res.render("weather", { cityList: cityList });
+});
+
+router.post("/add-city", function (req, res, next) {
+  cityList.push({
+    nom: req.body.cityadded,
+    image: "/images/picto-1.png",
+    descriptif: "Nuageux",
+    tempMin: 7,
+    tempMax: 10,
+  });
+
+  res.render("weather", { cityList: cityList });
+});
+
+router.get("/delete-city", function (req, res, next) {
+  cityList.splice(req.query.cityadded, 1);
   res.render("weather", { cityList: cityList });
 });
 
