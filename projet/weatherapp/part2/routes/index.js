43fe06151166ed94b3bd57273b7ade4b0537da88
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-04-05 15:05:33 +0200

    project done

--- projet/weatherapp/part2/routes/index.js
+++ projet/weatherapp/part2/routes/index.js
@@ -26,7 +26,7 @@ router.get("/", function (req, res, next) {
 
 router.get("/weather", function (req, res, next) {
   res.render("weather", {
-    cityList: cityList
+    cityList: cityList,
   });
 });
 
@@ -34,22 +34,22 @@ router.post("/add-city", function (req, res, next) {
   var dataAPI = req.body.cityadded;
   var requete = request("GET", `https://api.openweathermap.org/data/2.5/weather?q=${dataAPI}&lang=fr&units=metric&appid=a6e3eaa80322d67999b7ac143be3fddb`);
   var resultWS = JSON.parse(requete.body);
-  console.log(resultWS.weather[0].description);
+  console.log(resultWS.cod);
   var cityAlreadyAdded = false;
   for (var i = 0; i < cityList.length; i++) {
     if (cityList[i].nom == req.body.cityadded) {
       cityAlreadyAdded = true;
     }
   }
-  if (cityAlreadyAdded == false) {
+  if (cityAlreadyAdded == false && resultWS.cod === 200) {
     cityList.push({
-      nom: req.body.cityadded,
+      nom: resultWS.name,
       image: `http://openweathermap.org/img/wn/${resultWS.weather[0].icon}.png`,
       descriptif: resultWS.weather[0].description,
       tempMin: resultWS.main.temp_min,
       tempMax: resultWS.main.temp_max,
     });
-  }
+  } else {}
   res.render("weather", {
     cityList: cityList,
     resultWS: resultWS,
