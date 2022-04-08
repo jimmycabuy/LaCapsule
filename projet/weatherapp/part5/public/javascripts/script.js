b644f67afda3353d3b4b8d65d5030ec9708f43a4
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-04-08 12:58:02 +0200

    project wheater point 4 done

--- projet/weatherapp/part5/public/javascripts/script.js
+++ projet/weatherapp/part5/public/javascripts/script.js
@@ -8,14 +8,26 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(mymap);
 
 
-
-
 var cityFromData = document.getElementsByClassName("city-card");
 var nameFromData = document.getElementsByClassName("city-card");
+
+// var customIcon = L.icon({
+//     iconUrl: './images/marker.png',
+//     // shadowUrl: 'leaf-shadow.png',
+
+//     iconSize: [70, 70],
+//     shadowSize: [50, 64],
+
+//     iconAnchor: [22, 94],
+//     // shadowAnchor: [4, 62],
+
+//     popupAnchor: [20, -9]
+// });
+
 for (var i = 0; i < cityFromData.length; i++) {
 
     var nom = nameFromData[i].dataset.name;
     var longitude = cityFromData[i].dataset.lon;
     var lattitude = cityFromData[i].dataset.lat;
-    L.marker([lattitude, longitude]).addTo(mymap).bindPopup(nom);
+    L.marker([lattitude, longitude], /* {icon: customIcon} */).addTo(mymap).bindPopup(nom);
 }
