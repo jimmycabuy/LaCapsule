998e874794189c2e1bbda8828042e8b2772d008d
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-04-19 10:09:05 +0200

    mymoviz  day 2

--- projet/mymoviz/part2/src/App.js
+++ projet/mymoviz/part2/src/App.js
@@ -89,5 +89,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
---
+++ projet/mymoviz/part2/src/components/movie.jsx
@@ -0,0 +1,82 @@
+import "../stylesheet/movie.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardText,
+  Button,
+  Badge,
+  ButtonGroup,
+  CardGroup,
+} from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faVideo } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+function Movie(props) {
+  return (
+      <CardGroup className="card_movie col-12 col-lg-6 col-xl-4">
+          <Card>
+            <CardImg
+              alt={props.movieName}
+              src={props.movieImg}
+              top
+              width="100%"
+            />
+            <CardBody>
+              <CardText>
+              <h5 className="movie_title">{props.movieName}</h5>
+              </CardText>
+              <CardText className="card_text">
+                  <p>Favoris</p>
+                  <p className="heart"><FontAwesomeIcon icon={faHeart} /></p>
+              </CardText>
+              <CardText className="card_text">
+                <p>Nombres de vues</p>
+                <p><FontAwesomeIcon icon={faVideo} /></p>
+                <p><Badge color="secondary">{props.movieView}</Badge></p>
+              </CardText>
+              <CardText className="card_text">
+                  <p>Avis</p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p>({props.globalCountRating}/10)</p>
+                  <p>
+                    <ButtonGroup>
+                        <Button color="secondary">-</Button>
+                        <Button color="secondary">+</Button>
+                    </ButtonGroup></p>
+              </CardText>
+              <CardText className="card_text">
+                  <p>Moyenne</p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p className="star"><FontAwesomeIcon icon={faStar}/></p>
+                  <p>({props.globalRating}/10)</p>
+              </CardText>
+              <CardText> <p className="synopsis">Synopsis:</p>{props.movieDesc}</CardText>
+            </CardBody>
+          </Card>
+      </CardGroup>
+  );
+}
+
+export default Movie;
