76a734db0105978e8cec59fe3e912a115e33cc4f
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-04-22 11:03:17 +0200

    project mycontacts done

--- S5/mycontacts/src/App.js
+++ S5/mycontacts/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [dataStatus, setDataStatus] = useState([]);
+  useEffect(() => {
+    async function loadData() {
+      var rawResponse = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      var response = await rawResponse.json();
+      var allUsers = [];
+      for (var i = 0; i < response.length; i++) {
+        console.log(response[i].name);
+        allUsers.push(response[i].name);
+      }
+      setDataStatus(allUsers);
+    }
+    loadData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{dataStatus}</p>
     </div>
   );
 }
