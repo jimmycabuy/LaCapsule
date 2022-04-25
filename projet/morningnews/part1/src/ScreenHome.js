b806439989a700eb14a9f98b211a4bb4ac884bda
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-04-25 10:55:58 +0200

    new project : morningnews

--- projet/morningnews/part1/src/ScreenHome.js
+++ projet/morningnews/part1/src/ScreenHome.js
@@ -1,39 +1,34 @@
-import React from 'react';
-import './App.css';
-import {Input,Button} from 'antd';
+import React from "react";
+import "./App.css";
+import { Input, Button } from "antd";
 
 function ScreenHome() {
-
   return (
-    <div className="Login-page" >
+    <div className="Login-page">
+      {/* SIGN-IN */}
 
-          {/* SIGN-IN */}
+      <div className="Sign">
+        <Input className="Login-input" placeholder="arthur@lacapsule.com" />
 
-          <div className="Sign">
-                  
-                  <Input className="Login-input" placeholder="arthur@lacapsule.com" />
+        <Input.Password className="Login-input" placeholder="password" />
 
-                  <Input.Password className="Login-input" placeholder="password" />
-            
-
-            <Button style={{width:'80px'}} type="primary">Sign-in</Button>
-
-          </div>
-
-          {/* SIGN-UP */}
-
-          <div className="Sign">
-                  
-                  <Input className="Login-input" placeholder="Arthur G" />
-
-                  <Input.Password className="Login-input" placeholder="password" />
-            
-
-            <Button style={{width:'80px'}} type="primary">Sign-up</Button>
-
-          </div>
-
+        <Button style={{ width: "80px" }} type="primary">
+          Sign-in
+        </Button>
       </div>
+
+      {/* SIGN-UP */}
+
+      <div className="Sign">
+        <Input className="Login-input" placeholder="Arthur G" />
+
+        <Input.Password className="Login-input" placeholder="password" />
+
+        <Button style={{ width: "80px" }} type="primary">
+          Sign-up
+        </Button>
+      </div>
+    </div>
   );
 }
 
