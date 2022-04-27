dfb502a7d709b6f649e2b4af1545f1dbe5e2dc7e
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-04-27 11:14:31 +0200

    project morning news part3 initialisation

---
+++ projet/6morningnews/part3/reactapp/src/components/ScreenHome.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import "../App.css";
+import { Input, Button } from "antd";
+
+function ScreenHome() {
+  // signUp useState
+  const [signUpUsername, setSignUpUsername] = useState("");
+  const [signUpEmail, setSignUpEmail] = useState("");
+  const [signUpPassword, setSgnUpPassword] = useState("");
+  const [isSign, setIsSign] = useState(false);
+  const [errorSignUp, setErrorSignUp] = useState("");
+
+  // signIn useState
+  const [signInUsername, setSignInUsername] = useState("");
+  const [signInPassword, setSgnInPassword] = useState("");
+  const [errorSignIn, setErrorSignIn] = useState("");
+
+  var handleSubmitSignUp = async () => {
+    const dataSignUp = await fetch ("/sign-up", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: `username=${signUpUsername}&email=${signUpEmail}&password=${signUpPassword}`,
+    })
+    const bodySignUp = await dataSignUp.json();
+    var messageErreurSignUp = bodySignUp.error;
+    setErrorSignUp(messageErreurSignUp);
+    if (bodySignUp.error.length === 0){
+      setIsSign(true);
+    }
+  };
+
+  var handleSubmitSignIn = async () => {
+    const dataSignIn = await fetch ("/sign-in", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: `username=${signInUsername}&password=${signInPassword}`,
+    })
+    
+    const bodySignIn = await dataSignIn.json();
+    var messageErreurSignIn = bodySignIn.error;
+    setErrorSignIn(messageErreurSignIn);
+    if (bodySignIn.user !== null){
+      setIsSign(true);
+    }
+  };
+
+  if(isSign){
+     return <Redirect to="/screensource"/>
+  }
+
+  return (
+    <div className="Login-page">
+      {/* SIGN-IN */}
+
+      <div className="Sign">
+      <span style={{ color:"red"}}>{errorSignIn}</span>
+        <Input style={{ marginBottom: "10px" }} className="Login-input" placeholder="username" onChange={(e) => setSignInUsername(e.target.value)}
+          value={signInUsername} />
+ 
+        <Input.Password style={{ marginBottom: "10px" }} className="Login-input" placeholder="password" onChange={(e) => setSgnInPassword(e.target.value)}
+          value={signInPassword} />
+
+        <Button onClick={() => handleSubmitSignIn()} >
+          Sign-in
+        </Button>
+      </div>
+
+      {/* SIGN-UP */}
+
+      <div className="Sign">
+      <span style={{ color:"red"}}>{errorSignUp}</span>
+        <Input style={{ marginBottom: "10px" }}
+          className="Login-input"
+          placeholder="username"
+          onChange={(e) => setSignUpUsername(e.target.value)}
+          value={signUpUsername}
+        />
+        <Input style={{ marginBottom: "10px" }} className="Login-input" placeholder="email" onChange={(e) => setSignUpEmail(e.target.value)}
+          value={signUpEmail} />
+        <Input.Password style={{ marginBottom: "10px" }} className="Login-input" placeholder="password" onChange={(e) => setSgnUpPassword(e.target.value)}
+          value={signUpPassword} />
+        
+        <Button onClick={() => handleSubmitSignUp()}>
+          Sign-up
+        </Button>
+        
+      </div>
+    </div>
+  );
+}
+
+export default ScreenHome;
---
+++ projet/6morningnews/part3/routes/index.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var router = express.Router();
+var userModel = require("../models/users");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+
+/* Sign-up */
+router.post("/sign-up", async function (req, res, next) {
+  let error = [];
+  const data = await userModel.findOne({email : req.body.email })
+  
+  if(data != null){
+    error.push("L'utilisateur est déjà inscrit")
+  }
+  if(req.body.username === "" || req.body.email === "" || req.body.password === ""){
+    error.push("Veuillez remplir tous les champs")
+  }
+
+  if (error.length == 0){
+  var newUser = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  await newUser.save();
+}
+  res.json({ newUser, error });
+  res.render("index");
+});
+
+/* Sign-in */
+router.post("/sign-in", async function (req, res, next) {
+  let error = [];
+  var user = await userModel.findOne({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  if(req.body.username === "" || req.body.password === ""){
+    error.push("Veuillez remplir tous les champs")
+  // } else if(!user && !req.body.password){
+  //   error.push("Mot de passe incorrect")
+  }else if(!user) {
+    error.push(`L'utilisateur ${req.body.username} n'existe pas`)
+  }
+
+  res.json({ user, error });
+  res.render("index");
+});
+
+module.exports = router;
