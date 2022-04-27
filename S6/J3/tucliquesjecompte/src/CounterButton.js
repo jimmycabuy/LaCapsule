18600691e026025d2afa6c3e74027e469bfaaa45
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-04-27 10:49:08 +0200

    challenge redux

--- S6/J3/tucliquesjecompte/src/CounterButton.js
+++ S6/J3/tucliquesjecompte/src/CounterButton.js
@@ -4,16 +4,26 @@ import './App.css';
 
 function CounterButton(props) {
   return (
-    <button className="btn" onClick={ props.onIncreaseClick }>Increase</button>
+    <p style={{display: "flex", flexDirection:"column", gap:"10px" }}>
+      <button className="btn" onClick={ props.onIncreaseClick }>Increase</button>
+      <button className="btn" onClick={ props.onDecreaseClick }>Decrease</button>
+      <button className="btn" onClick={ props.onResetClick }>Reset</button>
+    </p>
   )
 
 }
-
 
 function mapDispatchToProps(dispatch) {
   return {
     onIncreaseClick: function() { 
         dispatch( {type: 'increase'} ) 
+    },
+    onDecreaseClick: function() { 
+        dispatch( {type: 'decrease'} ) 
+    },
+  
+    onResetClick: function() { 
+        dispatch( {type: 'reset'} ) 
     }
   }
 }
--- S6/J3/tucliquesjecompte/src/count.reducer.js
+++ S6/J3/tucliquesjecompte/src/count.reducer.js
@@ -1,5 +1,11 @@
 export default function(count = 0, action) {
-    if(action.type == 'ajouter') {
+    if(action.type === 'decrease') {
+        return count - 1;
+    }
+    else if(action.type === 'reset') {
+        return count = 0;
+    }
+    else if(action.type === 'increase') {
         return count + 1;
     } else {
         return count;
