9e471f817c717a40c92428ef1b7f5230f644e363
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-04-29 10:06:32 +0200

    initialisation part5

--- projet/6morningnews/part5/models/users.js
+++ projet/6morningnews/part5/models/users.js
@@ -1,10 +1,17 @@
 var mongoose = require("mongoose");
 
+var wishlistSchema = mongoose.Schema({
+  title: String,
+  image : String,
+  description : String
+})
+
 var userSchema = mongoose.Schema({
   username: String,
   email: String,
   password: String,
-  token: String
+  token: String,
+  article : wishlistSchema
 });
 
 var userModel = mongoose.model("users", userSchema);
