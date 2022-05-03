4980efdf7ce9ce9ed105180dc1f1a6f6098ea813
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-05-03 10:16:37 +0200

    initiliasation

---
+++ S7/reactnavigation-stack-demo/components/PageAScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class PageAScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>PageAScreen</Text>
+      </View>
+    )
+  }
+}
